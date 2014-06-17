215d9c902c7ad02ae8c345fd7f3a49bedf14344a
Jayaram <jayaramimca@gmail.com>
2014-06-18 02:17:07 +0530

    Settings

--- account/profile_settings/ProfileSettingsView.py
+++ account/profile_settings/ProfileSettingsView.py
@@ -3,13 +3,21 @@ import logging
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 from django.views.generic.base import View
+
+from account.constant import Constants
 from custom_mixins.LoginCheckMixin import LoginCheckMixin
+from user_management.UserMixin import findOrUpdateBasicProfileCache
+
 
 log = logging.getLogger(__name__)
 class ProfileSettingsView(LoginCheckMixin, View):
     
     def get(self, request):
-        context = RequestContext(request, {});
+        profile_id = request.session.get(Constants.PRF_ID);
+        from django.core.cache import cache
+        state = cache.get(profile_id +Constants.STATE_POST_FIX);
+        state['usr'] = findOrUpdateBasicProfileCache(profile_id);
+        context = RequestContext(request, state);
         log.info('[END]- Welcome View rendered.');
         return render_to_response('profile/settings/home.html',context);
         pass
