0ea39b3a70ee91bee6edbc2ce59d3ae2fd7a814c
Jayaram <jayaramimca@gmail.com>
2014-06-21 00:18:13 +0530

    : New Task
    Task-Url:

--- account/account/static/js/settings/uim.settings.js
+++ account/account/static/js/settings/uim.settings.js
@@ -1,4 +1,10 @@
-$("#menu-toggle").click(function(e) {
+$("#uimLeftMenuTogl").click(function(e) {
         e.preventDefault();
+        var togel_ico = $('#uimLeftMenuToglIco');
+        if(togel_ico.hasClass('glyphicon-indent-left')){
+        	togel_ico.removeClass('glyphicon-indent-left').addClass('glyphicon-indent-right');
+        }else{
+        	togel_ico.removeClass('glyphicon-indent-right').addClass('glyphicon-indent-left');
+        }
         $("#wrapper").toggleClass("active");
 });
