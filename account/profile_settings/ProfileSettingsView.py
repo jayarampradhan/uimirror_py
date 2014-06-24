92b6dba59c17c53697a18dfcee21018fcf6d3053
Jayaram <jayaramimca@gmail.com>
2014-06-24 10:09:28 +0530

    : New Task
    Task-Url:

--- account/profile_settings/ProfileSettingsView.py
+++ account/profile_settings/ProfileSettingsView.py
@@ -12,13 +12,23 @@ from user_management.UserMixin import findOrUpdateBasicProfileCache
 log = logging.getLogger(__name__)
 class ProfileSettingsView(LoginCheckMixin, View):
     
-    def get(self, request):
+    def get(self, request, app = 'social'):
+        log.info('[START]- Request Received for the settings Page');
+        tab = request.GET.get('ref', self.determineDefaultTab(app));
         profile_id = request.session.get(Constants.PRF_ID);
         from django.core.cache import cache
-        state = cache.get(profile_id +Constants.STATE_POST_FIX);
-        state['usr'] = findOrUpdateBasicProfileCache(profile_id);
-        context = RequestContext(request, state);
-        log.info('[END]- Welcome View rendered.');
+        # TODO: Get the user settings by app name
+        res = {}
+        res['usr'] = findOrUpdateBasicProfileCache(profile_id);
+        tab_data = {}
+        tab_data[tab] = 'test'
+        res[app] = tab_data
+        log.debug(res); 
+        context = RequestContext(request, res);
+        log.info('[START]- Request Rendered for the settings Page');
         return render_to_response('profile/settings/social/home.html',context);
-        pass
-        # TODO: Complete This Functionality with UI 
+        # TODO: Complete This Functionality with UI
+        
+    def determineDefaultTab(self, appname):
+        if appname == 'social':
+            return 'chat'; 
--- account/profile_settings/urls.py
+++ account/profile_settings/urls.py
@@ -3,4 +3,5 @@ from profile_settings.ProfileSettingsView import ProfileSettingsView
 
 urlpatterns = patterns('',
     url(r'^$', ProfileSettingsView.as_view(), name='uim.profile.setting'),
+    url(r'^(?P<app>\w+)/$', ProfileSettingsView.as_view(), name='uim.profile.setting.by.app'),
 )
